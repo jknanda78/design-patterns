fbcd0dc62114a0520054cda54f3d83e19e327889
jnanda <jyoti.kaustuv.nanda@gmail.com>
2017-09-30 11:21:07 -0700

    style fixes + render markup for button only

--- src/js/components/input-field-patterns.js
+++ src/js/components/input-field-patterns.js
@@ -2,24 +2,33 @@ import { h } from 'preact';
 import '../../scss/atom-input-field.scss';
 import '../../scss/vault-input-field.scss';
 
+export const renderFormField = props => {
+	if (props.label) {
+		return (
+			<div class="group">
+				{props.children}
+				<span class="highlight" />
+				<span class="bar" />
+				<label for={props.name}>{props.label}</label>
+			</div>
+		);
+	}
+
+	return (
+		<div class="btn-group">
+			{props.children}
+		</div>
+	);
+};
+
 export const AtomDInputField = props => (
 	<div class="atom-input-field">
-		<div class="group">
-			{props.children}
-			<span class="highlight" />
-			<span class="bar" />
-			<label for={props.name}>{props.label}</label>
-		</div>
+		{ renderFormField(props) }
 	</div>
 );
 
 export const VaultDInputField = props => (
 	<div class="vault-input-field">
-		<div class="group">
-			{props.children}
-			<span class="highlight" />
-			<span class="bar" />
-			<label for={props.name}>{props.label}</label>
-		</div>
+		{ renderFormField(props) }
 	</div>
 );
